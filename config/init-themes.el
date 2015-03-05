090c2f06b5378e505be749d0314c4459d867da5c
doraemonext <doraemonext@gmail.com>
2015-03-05 16:33:30 +0800

    Fix: 修正终端下的主题选择

--- config/init-themes.el
+++ config/init-themes.el
@@ -1,10 +1,9 @@
 ;; 主题设置
-
+(require 'darcula-theme)
 
 ;; 字体设置
 (if (display-graphic-p)
     (progn
-      (require 'darcula-theme)
       (set-face-attribute 'default nil :font "Monaco 12")
       (dolist (charset '(kana han symbol cjk-misc bopomofo))
         (set-fontset-font (frame-parameter nil 'font)
